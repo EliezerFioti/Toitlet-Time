186ea46951826114ca51546174a4e7a391db7ce0
EliezerFioti <eliezerfioti@gmail.com>
2024-02-27 21:52:51 -0500

    Added additional JUnit tests for comprehensive test coverage. Expanded the test suite in ToiletTimeTest to include more scenarios for getNumOfPoopsPerDay, getNumOfPoopsPerWeek, getNumOfPoopsPerMonth, and isDateInTheFuture methods. These tests cover various edge cases, ensuring the robustness and reliability of the codebase. This commit enhances the overall test coverage and helps maintain code quality.

--- src/ToiletTimeTest.java
+++ src/ToiletTimeTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class ToiletTimeTest {
 	
 	@Test 
-	public void singlePoopPerDayTest() {
+	public void getNumOfPoopsPerDay_SinglePoopPerDayTest() {
 		var toilet = new ToiletTime();
 		LocalDate poopDate = LocalDate.now();
 		
@@ -17,10 +17,11 @@ class ToiletTimeTest {
 	}
 
 	@Test
-	public void multiplePoopsPerDayTest() {
+	public void getNumOfPoopsPerDay_MultiplePoopsPerDayTest() {
 		var toilet = new ToiletTime();
 		LocalDate poopDate = LocalDate.now();
 		
+		// Log multiple poops for different dates within the same day
 		toilet.didYouPoop(true, poopDate);
 		toilet.didYouPoop(true, poopDate);
 		toilet.didYouPoop(true, poopDate);
@@ -32,9 +33,25 @@ class ToiletTimeTest {
 	}
 	
 	@Test
-	public void multiplePoopsPerWeekTest() {
+	public void getNumOfPoopsPerDay_NoPoopsPerdayTest() {
+		var toilet = new ToiletTime();
+		LocalDate poopDate = LocalDate.now();
+		
+		toilet.didYouPoop(false, poopDate);
+		toilet.didYouPoop(false, poopDate);
+		toilet.didYouPoop(false, poopDate);
+		toilet.didYouPoop(false, poopDate);
+		toilet.didYouPoop(false, poopDate);
+		toilet.didYouPoop(false, poopDate);
+		
+		assertEquals(0, toilet.getNumOfPoopsPerDay(poopDate.getDayOfWeek()), "The expected number of times pooped today: 0");
+	}
+	
+	@Test
+	public void getNumOfPoopsPerWeek_MultiplePoopsPerWeekTest() {
 		var toilet = new ToiletTime();
 		
+		// Log multiple poops for different dates within the same week
 		toilet.didYouPoop(true, LocalDate.of(2024, 1, 28));
 		toilet.incrementNumOfPoopsPerWeek();
 		toilet.didYouPoop(true, LocalDate.of(2024, 1, 28));
@@ -49,6 +66,14 @@ class ToiletTimeTest {
 		toilet.incrementNumOfPoopsPerWeek();
 		toilet.didYouPoop(true, LocalDate.of(2024, 1, 31));
 		toilet.incrementNumOfPoopsPerWeek();
+
+		assertEquals(7, toilet.getNumOfPoopsPerWeek(), "The expected number of times pooped today: 7");
+	}
+	
+	@Test
+	public void getNumOfPoopsPerWeek_NoPoopsPerWeekTest() {
+		var toilet = new ToiletTime();
+		
 		toilet.didYouPoop(false, LocalDate.of(2022, 1, 27));
 		toilet.didYouPoop(false, LocalDate.of(2022, 2, 28));
 		toilet.didYouPoop(false, LocalDate.of(2023, 3, 29));
@@ -60,34 +85,73 @@ class ToiletTimeTest {
 		toilet.didYouPoop(false, LocalDate.of(2026, 1, 28));
 		toilet.didYouPoop(false, LocalDate.of(2026, 1, 29));
 
-		assertEquals(7, toilet.getNumOfPoopsPerWeek(), "The expected number of times pooped today: 7");
+		assertEquals(0, toilet.getNumOfPoopsPerWeek(), "The expected number of times pooped today: 0");
 	}
 	
 	@Test
-	public void multiplePoopsPerMonthTest() {
+	public void getNumOfPoopsPerMonth_MultiplePoopsPerMonthTest() {
 	    var toilet = new ToiletTime();
 	    
+	    // Log multiple poops for different dates within the same month
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 1));
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 2));
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 2));
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 3));
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 3));
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 3));
-	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 4));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 4));    
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 5));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 6));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 6));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 7));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 8));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 8));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 9));	    
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 9));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 10));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 10));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 10));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 11));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 12));
+	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 12));
 	    
-	    assertEquals(7, toilet.getNumOfPoopsPerMonth(Month.JANUARY), "The expected number of times pooped this Month: 7");
+	    assertEquals(21, toilet.getNumOfPoopsPerMonth(Month.JANUARY), "The expected number of times pooped this Month: 21");
 	}
 	
 	@Test
-	public void multiplePoopsPerYearTest() {
+	public void getNumOfPoopsPerMonth_NoPoopsPerMonthTest() {
 	    var toilet = new ToiletTime();
 	    
-	    // Simulate multiple poops per year
-	    toilet.didYouPoop(false, LocalDate.of(2023, 4, 15));
-	    toilet.didYouPoop(false, LocalDate.of(2023, 5, 25));
-	    toilet.didYouPoop(false, LocalDate.of(2023, 5, 30));
-	    toilet.didYouPoop(false, LocalDate.of(2023, 6, 8));
-	    toilet.didYouPoop(false, LocalDate.of(2023, 7, 18));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 1));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 2));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 2));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 3));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 3));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 3));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 4));    
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 5));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 6));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 6));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 7));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 8));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 8));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 9));	    
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 9));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 10));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 10));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 10));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 11));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 12));
+	    toilet.didYouPoop(false, LocalDate.of(2024, 1, 12));
+	    
+	    assertEquals(0, toilet.getNumOfPoopsPerMonth(Month.JANUARY), "The expected number of times pooped this Month: 0");
+	}
+	
+	@Test
+	public void getNumOfPoopsPerMonth_MultiplePoopsPerYearTest() {
+	    var toilet = new ToiletTime();
+	    
+	    // Log multiple poops for different dates within the same year
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 1));
 	    toilet.incrementNumOfPoopsPerYear();
 	    toilet.didYouPoop(true, LocalDate.of(2024, 1, 15));
@@ -124,13 +188,38 @@ class ToiletTimeTest {
 	    toilet.incrementNumOfPoopsPerYear();
 	    toilet.didYouPoop(true, LocalDate.of(2024, 12, 14));
 	    toilet.incrementNumOfPoopsPerYear();
-	    toilet.didYouPoop(false, LocalDate.of(2025, 8, 30));
-	    toilet.didYouPoop(false, LocalDate.of(2025, 9, 12));
-	    toilet.didYouPoop(false, LocalDate.of(2025, 10, 6));
-	    toilet.didYouPoop(false, LocalDate.of(2025, 11, 22));
-	    toilet.didYouPoop(false, LocalDate.of(2025, 12, 14));
 	     
 	    assertEquals(18, toilet.getNumOfPoopsPerYear(), "The expected number of times pooped this year: 18");
 	}
+	
+	@Test 
+    public void isDateInTheFuture_OneDayInFuture() {
+        LocalDate futureDate = LocalDate.now().plusDays(1);
+        assertTrue(ToiletTime.isDateInTheFuture(futureDate));
+    }
+	
+	@Test 
+    public void isDateInTheFuture_FutureDate() {
+        LocalDate futureDate = LocalDate.now().plusYears(1);
+        assertTrue(ToiletTime.isDateInTheFuture(futureDate));
+    }
+	
+	@Test 
+    public void isDateInTheFuture_SameAsCurrentDate() {
+        LocalDate currentDate = LocalDate.now();
+        assertFalse(ToiletTime.isDateInTheFuture(currentDate));
+    }
+	
+	@Test 
+    public void isDateInTheFuture_OneDayInPast() {
+        LocalDate futureDate = LocalDate.now().minusDays(1);
+        assertFalse(ToiletTime.isDateInTheFuture(futureDate));
+    }
+
+    @Test 
+    public void isDateInTheFuture_VeryDistantPastDate() {
+        LocalDate distantPastDate = LocalDate.of(1000, 1, 1);
+        assertFalse(ToiletTime.isDateInTheFuture(distantPastDate));
+    }
 
 }
